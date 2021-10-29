3bfead2a920d0de7e7529763df811977d3a19bec
unknown <al.tsao.yw@gmail.com>
2021-10-29 12:58:50 +0800

    revise

---
+++ boxPlot.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from numpy.random import randn, randint, uniform, sample
+
+#資料匯入
+iris = sns.load_dataset('iris')
+
+df = iris.drop('species', axis = 1)
+
+df.plot(kind='box', figsize=(10,5), vert = False) #vert: 選擇水平或者垂直呈現
+
+plt.show()
